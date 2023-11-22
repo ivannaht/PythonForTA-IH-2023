bd84c632fa249f58bc78a19cdadb207808945d00
ihont <ihont@softserveinc.com>
2023-11-22 12:11:57 +0200

    [home-work-1] Fixed task 1.1

--- task_1_1.py
+++ task_1_1.py
@@ -4,6 +4,6 @@ age = int(input("HOW OLD ARE YOU?"))
 city: str = input("WHERE DO YOU LIVE?")
 
 
-print(f"HELLO, {name}")
+print(f"HELLO, {name.upper()}")
 print(f"YOUR AGE IS {age}")
-print(f"YOU LIVE IN {city}")
+print(f"YOU LIVE IN {city.upper()}")
