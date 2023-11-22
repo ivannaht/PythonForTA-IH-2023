dc49ca863fb61bfce6318685ffd3bb528617d065
ihont <ihont@softserveinc.com>
2023-11-22 20:02:22 +0200

    [home-work-1] Added task 1.2

---
+++ task_1_2.py
@@ -0,0 +1,26 @@
+def verify_arithmetic_operators():
+    a = float(input("Please enter the first number\n"))
+    b = float(input("Please enter the second number\n"))
+    if b < 0:
+        raise ValueError("the second number should be positive")
+    if b == 0:
+        raise ValueError("the second number should not be zero")
+    result = (f"a + b = {a + b}\n"
+              f"a - b = {a - b}\n"
+              f"a * b = {a * b}\n"
+              f"a / b = {a / b}\n"
+              f"a ** b = {a ** b}\n"
+              f"a // b = {a // b}\n"
+              f"a % b = {a % b}\n")
+    print(result.format(a, b))
+
+
+try:
+    verify_arithmetic_operators()
+except TypeError as e:
+    print(e)
+except ValueError as e:
+    print(e)
+finally:
+    print("verify_arithmetic_operators() function was run")
+
