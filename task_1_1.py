624391d7d12aedb8c7eba62335d4bef12282ba8d
ihont <ihont@softserveinc.com>
2023-11-22 11:38:55 +0200

    [home-work-1] Added task 1.1

---
+++ task_1_1.py
@@ -0,0 +1,8 @@
+name = input("WHAT IS YOUR NAME?")
+age = input("HOW OLD ARE YOU?")
+city = input("WHERE DO YOU LIVE?")
+
+
+print("HELLO, {}".format(name))
+print("YOUR AGE IS {}".format(age))
+print("YOU LIVE IN {}".format(city))
