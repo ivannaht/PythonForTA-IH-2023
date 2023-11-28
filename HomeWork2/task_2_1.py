6eedd926cf2968ba9c12e2774015a997f621af96
ihont <ihont@softserveinc.com>
2023-11-28 15:08:27 +0200

    [home-work-2] Updated task 2.1

--- HomeWork2/task_2_1.py
+++ HomeWork2/task_2_1.py
@@ -4,11 +4,21 @@ from functools import reduce
 def stat(numbers):
     result = []
     total_amount = len(numbers)
-    unique_amount = len(list(reduce(lambda nums, num: nums + [num] if num not in nums else nums, numbers, [])))
-    once_occure_amount = len(list(reduce(lambda nums, num: nums + [num] if numbers.count(num) == 1 else nums, numbers, [])))
-    max_occurance = max(list(reduce(lambda nums, num:  nums + [numbers.count(num)], numbers, [])))
-    max_occurance_elements_all = list(filter(lambda num: numbers.count(num) == max_occurance, numbers))
-    max_occurance_elements = sorted(list(reduce(lambda nums, num: nums + [num] if num not in nums else nums, max_occurance_elements_all, [])))
+    unique_amount = len(list(reduce(
+        lambda nums, num: nums + [num] if num not in nums else nums,
+        numbers, [])))
+    once_occure_amount = len(list(reduce(
+        lambda nums, num: nums + [num] if numbers.count(num) == 1 else nums,
+        numbers, [])))
+    max_occurance = max(list(reduce(
+        lambda nums, num:  nums + [numbers.count(num)],
+        numbers, [])))
+    max_occurance_elements_all = list(filter(
+        lambda num: numbers.count(num) == max_occurance,
+        numbers))
+    max_occurance_elements = sorted(list(reduce(
+        lambda nums, num: nums + [num] if num not in nums else nums,
+        max_occurance_elements_all, [])))
     result.append(total_amount)
     result.append(unique_amount)
     result.append(once_occure_amount)
