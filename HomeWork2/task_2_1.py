e54e7f275e70b5b59d4b2291b92091730e8a8874
ihont <ihont@softserveinc.com>
2023-11-28 14:24:05 +0200

    [home-work-2] Fixed task 2.1

--- HomeWork2/task_2_1.py
+++ HomeWork2/task_2_1.py
@@ -8,7 +8,7 @@ def stat(numbers):
     once_occure_amount = len(list(reduce(lambda nums, num: nums + [num] if numbers.count(num) == 1 else nums, numbers, [])))
     max_occurance = max(list(reduce(lambda nums, num:  nums + [numbers.count(num)], numbers, [])))
     max_occurance_elements_all = list(filter(lambda num: numbers.count(num) == max_occurance, numbers))
-    max_occurance_elements = list(reduce(lambda nums, num: nums + [num] if num not in nums else nums, max_occurance_elements_all, []))
+    max_occurance_elements = sorted(list(reduce(lambda nums, num: nums + [num] if num not in nums else nums, max_occurance_elements_all, [])))
     result.append(total_amount)
     result.append(unique_amount)
     result.append(once_occure_amount)
@@ -20,4 +20,3 @@ def stat(numbers):
 
 print(stat([1, 9, 9, 9, 9, 9, 9, 2, 2, 2, 2, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 6, 7, 8]))
 print(stat([-1, 0, 1]))
-
--- HomeWork2/test_task_2_1.py
+++ HomeWork2/test_task_2_1.py
@@ -6,7 +6,7 @@ from HomeWork2.task_2_1 import stat
 
 @pytest.mark.parametrize("input_value, output_value",
                          [
-                             ([1, 9, 9, 9, 9, 9, 9, 2, 2, 2, 2, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 6, 7, 8], [29, 9, 4, [9, 2, 5], 6]),
+                             ([1, 9, 9, 9, 9, 9, 9, 2, 2, 2, 2, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 6, 7, 8], [29, 9, 4, [2, 5, 9], 6]),
                              ([-1, 0, 1], [3, 3, 3, [-1, 0, 1], 1])
                          ])
 def test_stat(input_value, output_value):
