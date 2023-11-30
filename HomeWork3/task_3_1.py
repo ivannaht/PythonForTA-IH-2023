ca9b0413c62b008539853af46c9f74cbecbf1483
ihont <ihont@softserveinc.com>
2023-12-01 00:49:31 +0200

    [home-work-3] Updated task 3.1

--- HomeWork3/task_3_1.py
+++ HomeWork3/task_3_1.py
@@ -10,20 +10,23 @@ def calculate_cubes_sum(numbers):
 
 def convert(str_main: str, n: int) -> str:
     """convert function for strings"""
-    count = len(str_main) // n
-    first = 0
-    last = n
-    result = ''
-    for i in range(0, count):
-        result_str = str_main[first:last]
-        result_list = list(str_main[first:last])
-        if calculate_cubes_sum(result_list) % 2 == 0:
-            result += result_str[::-1]
-        else:
-            result += result_str[1:] + result_str[0]
-        first += n
-        last += n
-    return result
+    if n == 0:
+        return ""
+    else:
+        count = len(str_main) // n
+        first = 0
+        last = n
+        result = ''
+        for i in range(0, count):
+            result_str = str_main[first:last]
+            result_list = list(str_main[first:last])
+            if calculate_cubes_sum(result_list) % 2 == 0:
+                result += result_str[::-1]
+            else:
+                result += result_str[1:] + result_str[0]
+            first += n
+            last += n
+        return result
 
 
 print(convert("123456987654", 6))
--- HomeWork3/test_task_3_1.py
+++ HomeWork3/test_task_3_1.py
@@ -6,7 +6,14 @@ from HomeWork3.task_3_1 import convert
 
 @pytest.mark.parametrize("input_value1, input_value2, output_value", [
                         ("123456987654", 6, "234561876549"),
-                        ("123456987653", 6, "234561356789")
+                        ("123456987653", 6, "234561356789"),
+                        ("66443875", 4, "44668753"),
+                        ("66443875", 8, "64438756"),
+                        ("66443876 9", 8, "67834466"),
+                        ("123456779", 8, "23456771"),
+                        ("", 8, ""),
+                        ("123456779", 0, ""),
+                        ("563000655734469485", 4, "0365065073456944")
                          ])
 def test_convert(input_value1, input_value2, output_value):
     """verify convert function"""
