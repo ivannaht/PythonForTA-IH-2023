82c048ce752caf95a7b99a81953f59a5ba7c06df
ihont <ihont@softserveinc.com>
2023-12-01 11:02:53 +0200

    [home-work-3] Fixed task 3.1

--- HomeWork3/task_3_1.py
+++ HomeWork3/task_3_1.py
@@ -12,21 +12,21 @@ def convert(str_main: str, n: int) -> str:
     """convert function for strings"""
     if n == 0:
         return ""
-    else:
-        count = len(str_main) // n
-        first = 0
-        last = n
-        result = ""
-        for i in range(0, count):
-            result_str = str_main[first:last]
-            result_list = list(str_main[first:last])
-            if calculate_cubes_sum(result_list) % 2 == 0:
-                result += result_str[::-1]
-            else:
-                result += result_str[1:] + result_str[0]
-            first += n
-            last += n
-        return result
+
+    count = len(str_main) // n
+    first = 0
+    last = n
+    result = ""
+    for i in range(0, count):
+        result_str = str_main[first:last]
+        result_list = list(str_main[first:last])
+        if calculate_cubes_sum(result_list) % 2 == 0:
+            result += result_str[::-1]
+        else:
+            result += result_str[1:] + result_str[0]
+        first += n
+        last += n
+    return result
 
 
 print(convert("123456987654", 6))
