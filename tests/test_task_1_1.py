4a0f0c655214a01c231fa8b3cd4d96727a4a2a87
ihont <ihont@softserveinc.com>
2023-12-05 13:59:32 +0200

    [home-work-4] Added custom exception

--- tests/test_task_1_1.py
+++ tests/test_task_1_1.py
@@ -4,8 +4,8 @@ from helpers.custom_exceptions import InsufficientAgeError
 from tasks.task_1_1 import show_user_info
 
 data = [
-        ("First", 5, "Second", InsufficientAgeError)
-        # ("a", "b", "c", ValueError)
+        ("First", 5, "Second", InsufficientAgeError),
+        ("a", "b", "c", ValueError)
 ]
 
 @pytest.mark.parametrize("name, age, city, e", data)
