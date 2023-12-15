90fd15731c0208ffc8c75ade01fd191441f0e8c6
ihont <ihont@softserveinc.com>
2023-12-15 12:50:07 +0200

    [home-work-6] Added decorator

--- tasks/task_1_2.py
+++ tasks/task_1_2.py
@@ -4,6 +4,23 @@ input_a = float(input("Please enter the first number\n"))
 input_b = float(input("Please enter the second number\n"))
 
 
+def decorator_with_arguments(outer_message, start_message, finish_message):
+    def decorator(fun):
+        print(outer_message)
+
+        def wrapper_accepting_arguments(a, b):
+            print(start_message)
+            print(f"The arguments are: {a}, {b}")
+            print(fun(a, b))
+            print(finish_message)
+
+        return wrapper_accepting_arguments
+
+    return decorator
+
+
+@decorator_with_arguments("decorator for function verify_arithmetic_operators",
+                          "started", "finished")
 def verify_arithmetic_operators(a, b):
     if b < 0:
         raise NegativeValueError(b)
@@ -16,12 +33,10 @@ def verify_arithmetic_operators(a, b):
               f"a ** b = {a ** b}\n"
               f"a // b = {a // b}\n"
               f"a % b = {a % b}\n")
-    print(result)
+    return result
 
 
 try:
     verify_arithmetic_operators(input_a, input_b)
 except ValueError as e:
     print(e)
-finally:
-    print("verify_arithmetic_operators() function was run")
