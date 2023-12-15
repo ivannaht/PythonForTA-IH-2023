364ec6b79168652b0ef30b8fbe7687cf939a4dd1
ihont <ihont@softserveinc.com>
2023-12-15 19:19:58 +0200

    [home-work-6] Updated decorator

---
+++ helpers/logger.py
@@ -0,0 +1,18 @@
+from time import perf_counter
+
+
+def logger_with_arguments(outer_message, start_message, finish_message):
+    """decorator for functions with any arguments"""
+    def decorator(fun):
+        print(f"{outer_message} {fun.__name__}")
+
+        def wrapper_accepting_arguments(*args, **kwargs):
+            start_time = perf_counter()
+            print(start_message)
+            print(f"The arguments are: {args}")
+            print(fun(*args, **kwargs))
+            print(f"{finish_message} in {(perf_counter() - start_time): .6f} seconds")
+
+        return wrapper_accepting_arguments
+
+    return decorator
--- tasks/task_1_2.py
+++ tasks/task_1_2.py
@@ -1,26 +1,12 @@
 from helpers.custom_exceptions import NegativeValueError
+from helpers.logger import logger_with_arguments
 
 input_a = float(input("Please enter the first number\n"))
 input_b = float(input("Please enter the second number\n"))
 
 
-def decorator_with_arguments(outer_message, start_message, finish_message):
-    def decorator(fun):
-        print(outer_message)
-
-        def wrapper_accepting_arguments(a, b):
-            print(start_message)
-            print(f"The arguments are: {a}, {b}")
-            print(fun(a, b))
-            print(finish_message)
-
-        return wrapper_accepting_arguments
-
-    return decorator
-
-
-@decorator_with_arguments("decorator for function verify_arithmetic_operators",
-                          "started", "finished")
+@logger_with_arguments("logger for function",
+                       "started", "finished")
 def verify_arithmetic_operators(a, b):
     if b < 0:
         raise NegativeValueError(b)
