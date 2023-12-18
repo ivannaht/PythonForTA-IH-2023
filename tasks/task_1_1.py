8cc1d6d404855510c324e8c3d0607cb289aa151f
ihont <ihont@softserveinc.com>
2023-12-18 18:35:32 +0200

    [refactoring-2] Added class User task 1.1

--- tasks/task_1_1.py
+++ tasks/task_1_1.py
@@ -1,23 +1,31 @@
 from helpers.custom_exceptions import InsufficientAgeError, EmptyValueError
 
-input_name = input("WHAT IS YOUR NAME?\n")
-input_age = input("HOW OLD ARE YOU?\n")
-input_city = input("WHERE DO YOU LIVE?\n")
+
+class User:
+
+    def __init__(self, name, age, city):
+        self.name = name
+        self.age = age
+        self.city = city
+
+    def get_data(self):
+        self.name = input("WHAT IS YOUR NAME?\n")
+        self.age = input("HOW OLD ARE YOU?\n")
+        self.city = input("WHERE DO YOU LIVE?\n")
+
+    def show_user_info(self):
+        min_age = 18
+        if self.name == "" or self.city == "" or self.age == "":
+            raise EmptyValueError
+        if not self.age.isdigit():
+            raise ValueError("age must be a number")
+        if int(self.age) < min_age:
+            raise InsufficientAgeError(self.age, min_age)
+        return f"HELLO, {self.name.upper()}! YOUR AGE IS {int(self.age)}. YOU LIVE IN {self.city.upper()}."
 
 
-def show_user_info(name, age, city):
-    min_age = 18
-
-    if name == "" or city == "" or age == "":
-        raise EmptyValueError
-
-    if not age.isdigit():
-        raise ValueError("age must be a number")
-
-    if int(age) < min_age:
-        raise InsufficientAgeError(age, min_age)
-
-    return f"HELLO, {name.upper()}! YOUR AGE IS {int(age)}. YOU LIVE IN {city.upper()}."
-
-
-print(show_user_info(input_name, input_age, input_city))
+user1 = User("Anna", "25", "London")
+user2 = User("", "", "")
+user2.get_data()
+print(user1.show_user_info())
+print(user2.show_user_info())
