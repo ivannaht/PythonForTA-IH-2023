a536c468df0824beaf81926467010f7a4166920c
ihont <ihont@softserveinc.com>
2023-12-18 18:54:54 +0200

    [refactoring-2] Added docstring to task 1.1

--- tasks/task_1_1.py
+++ tasks/task_1_1.py
@@ -1,19 +1,24 @@
+"""Module for User class"""
 from helpers.custom_exceptions import InsufficientAgeError, EmptyValueError
 
 
 class User:
+    """Class for user"""
 
     def __init__(self, name, age, city):
+        """Constructor for user"""
         self.name = name
         self.age = age
         self.city = city
 
     def get_data(self):
+        """Method for getting user data from input"""
         self.name = input("WHAT IS YOUR NAME?\n")
         self.age = input("HOW OLD ARE YOU?\n")
         self.city = input("WHERE DO YOU LIVE?\n")
 
     def show_user_info(self):
+        """Method for showing formatted user info"""
         min_age = 18
         if self.name == "" or self.city == "" or self.age == "":
             raise EmptyValueError
