5ee57988fadbc335f33c3a59b65c3ec0e1a60aa0
ihont <ihont@softserveinc.com>
2023-12-31 12:55:39 +0200

    [refactoring-3] Moved JsonFileEditor class to separate file

---
+++ tasks/json_file_editor.py
@@ -0,0 +1,28 @@
+import json
+from pathlib import Path
+
+
+class JsonFileEditor:
+    """Parent class for reading and modifying JSON file"""
+
+    def __init__(self, json_file="geometric_shapes.json", directory="assets"):
+        """Constructor for user"""
+        base_dir = Path(__file__).resolve().parent.parent
+        self.data_file = base_dir.joinpath(directory).joinpath(json_file)
+        self.data = self.get_items
+
+    @property
+    def get_items(self):
+        """function for reading items from JSON file"""
+        with open(self.data_file) as file:
+            return json.load(file)
+
+    def generate_new_item(self):
+        raise NotImplementedError("Child class must implement this abstract method")
+
+    def add_item(self):
+        """function for writing items in JSON file"""
+        with open(self.data_file, mode="w") as file:
+            new_item = self.generate_new_item()
+            self.data.append(new_item)
+            json.dump(self.data, file, indent=4)
--- tasks/task_7_1.py
+++ tasks/task_7_1.py
@@ -1,33 +1,6 @@
-import json
 from collections import namedtuple
-from pathlib import Path
 import random
-
-
-class JsonFileEditor:
-    """Parent class for reading and modifying JSON file"""
-
-    def __init__(self, json_file="geometric_shapes.json", directory="assets"):
-        """Constructor for user"""
-        base_dir = Path(__file__).resolve().parent.parent
-        self.data_file = base_dir.joinpath(directory).joinpath(json_file)
-        self.data = self.get_items
-
-    @property
-    def get_items(self):
-        """function for reading items from JSON file"""
-        with open(self.data_file) as file:
-            return json.load(file)
-
-    def generate_new_item(self):
-        raise ValueError("The item should be generated in Child class")
-
-    def add_item(self):
-        """function for writing items in JSON file"""
-        with open(self.data_file, mode="w") as file:
-            new_item = self.generate_new_item()
-            self.data.append(new_item)
-            json.dump(self.data, file, indent=4)
+from tasks.json_file_editor import JsonFileEditor
 
 
 class GeometricShapesFileEditor(JsonFileEditor):
