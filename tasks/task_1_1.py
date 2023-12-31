288f5a34cc752b8feb7bd772dfc52b262b549325
ihont <ihont@softserveinc.com>
2023-12-31 20:35:47 +0200

    [home-work-8] Added phone to User class

--- tasks/task_1_1.py
+++ tasks/task_1_1.py
@@ -1,39 +1,55 @@
 """Module for User class"""
+import re
+
 from helpers.custom_exceptions import InsufficientAgeError, EmptyValueError
 
 
 class User:
     """Class for user"""
 
-    def __init__(self, name, age, city):
+    def __init__(self, name, age, city, phone="+1 (123) 456-7890"):
         """Constructor for user"""
+        self.validate_user_info(name, city, age)
         self.name = name
         self.age = age
         self.city = city
+        self.phone = phone
 
-    def get_data(self):
+    def input_user_data(self):
         """Method for getting user data from input"""
         self.name = input("WHAT IS YOUR NAME?\n")
         self.age = input("HOW OLD ARE YOU?\n")
         self.city = input("WHERE DO YOU LIVE?\n")
+        self.phone = input("WHAT IS YOUR PHONE NUMBER?\n")
+
+    @staticmethod
+    def validate_user_info(name, city, age):
+        """validate user info using regular expressions"""
+        min_age = 18
+        pattern = re.compile(r"^([a-z]+)([-'][a-z]+)*(\-[a-z]+)?(( [a-z]+)?([-'][a-z]+)*){0,3}[a-z]*$", re.IGNORECASE)
+        if name == "" or city == "" or age == "":
+            raise EmptyValueError
+        if not pattern.search(name):
+            raise ValueError("name must include only letters, spaces, apostrophes, or hyphens")
+        if not pattern.search(city):
+            raise ValueError("city must include only letters, spaces, apostrophes, or hyphens")
+        if not age.isdigit():
+            raise ValueError("age must be a number")
+        if int(age) < min_age:
+            raise InsufficientAgeError(age, min_age)
 
     def show_user_info(self):
         """Method for showing formatted user info"""
-        min_age = 18
-        if self.name == "" or self.city == "" or self.age == "":
-            raise EmptyValueError
-        if not self.age.isdigit():
-            raise ValueError("age must be a number")
-        if int(self.age) < min_age:
-            raise InsufficientAgeError(self.age, min_age)
+
         return (f"HELLO, {self.name.upper()}! "
                 f"YOUR AGE IS {int(self.age)}. "
-                f"YOU LIVE IN {self.city.upper()}.")
+                f"YOU LIVE IN {self.city.upper()}. "
+                f"YOUR PHONE NUMBER IS {self.phone}.")
 
 
 user1 = User("Anna", "25", "London")
 print(user1.show_user_info())
 
-user2 = User("", "", "")
-user2.get_data()
+user2 = User("a", "18", "b")
+# user2.input_user_data()
 print(user2.show_user_info())
--- tests/test_task_1_1.py
+++ tests/test_task_1_1.py
@@ -3,18 +3,45 @@ import pytest
 from helpers.custom_exceptions import InsufficientAgeError, EmptyValueError
 from tasks.task_1_1 import User
 
-data = [
-    ("first", "5", "second", InsufficientAgeError),
-    ("a", "b", "c", ValueError),
-    ("", "20", "test", EmptyValueError),
-    ("test", "20", "", EmptyValueError),
-    ("first", "", "second",  EmptyValueError),
+data_1 = [
+    ("first", "5", "second", "+3 (805) 055-5555", InsufficientAgeError),
+    ("a", "b", "c", "+3 (805) 055-5555", ValueError),
+    ("", "20", "test", "+3 (805) 055-5555", EmptyValueError),
+    ("test", "20", "", "+3 (805) 055-5555", EmptyValueError),
+    ("first", "", "second", "+3 (805) 055-5555", EmptyValueError),
+    ("9ds *", "20", "Lviv", "+3 (805) 055-5555", ValueError),
+    ("ds fdf9", "20", "Lviv", "+3 (805) 055-5555", ValueError),
+    ("d's f-f j'j hh ss gt i", "20", "Lviv", "+3 (805) 055-5555", ValueError),
+    ("abc", "20", "Lviv*fdf P8fdfd", "+3 (805) 055-5555", ValueError),
+    ("abc", "20", "Lviv''fdf P--fdfd", "+3 (805) 055-5555", ValueError),
+    ("abc", "20", "'Paris-", "+3 (805) 055-5555", ValueError)
 ]
 
 
-@pytest.mark.parametrize("name, age, city, e", data)
-def test_show_user_info_with_exceptions(name, age, city, e):
+@pytest.mark.parametrize("name, age, city, phone, e", data_1)
+def test_show_user_info_with_exceptions(name, age, city, phone, e):
     """verify show_user_info function"""
     with pytest.raises(e):
-        user = User(name, age, city)
-        user.show_user_info()
+        user1 = User(name, age, city, phone)
+        user1.show_user_info()
+
+
+data_2 = [
+    ("Anna", "25", "London", "+3 (123) 456-7890",
+     "HELLO, ANNA! YOUR AGE IS 25. YOU LIVE IN LONDON. YOUR PHONE NUMBER IS +3 (123) 456-7890."),
+    ("Oo'hgh H-JH", "20", "London", "+3 (123) 456-7890",
+     "HELLO, OO'HGH H-JH! YOUR AGE IS 20. YOU LIVE IN LONDON. YOUR PHONE NUMBER IS +3 (123) 456-7890."),
+    ("O-o l'k", "20", "London", "+3 (123) 456-7890",
+     "HELLO, O-O L'K! YOUR AGE IS 20. YOU LIVE IN LONDON. YOUR PHONE NUMBER IS +3 (123) 456-7890."),
+    ("O-o'p l'k j'k-o h'h", "20", "London", "+3 (123) 456-7890",
+     "HELLO, O-O'P L'K J'K-O H'H! YOUR AGE IS 20. YOU LIVE IN LONDON. YOUR PHONE NUMBER IS +3 (123) 456-7890."),
+    ("Anna", "25", "London-l'p New-y't Old'oo'wd-i", "+3 (123) 456-7890",
+     "HELLO, ANNA! YOUR AGE IS 25. YOU LIVE IN LONDON-L'P NEW-Y'T OLD'OO'WD-I. YOUR PHONE NUMBER IS +3 (123) 456-7890.")
+]
+
+
+@pytest.mark.parametrize("name, age, city, phone, expected_result", data_2)
+def test_show_user_info(name, age, city, phone, expected_result):
+    """verify show_user_info function"""
+    user2 = User(name, age, city, phone)
+    assert user2.show_user_info() == expected_result
