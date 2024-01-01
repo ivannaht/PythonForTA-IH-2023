9c10fed3123584dbca75498df9e606fc0fffb084
ihont <ihont@softserveinc.com>
2024-01-01 19:24:57 +0200

    [home-work-8] Updated tests cloth shop JSON

--- tasks/task_7_3.py
+++ tasks/task_7_3.py
@@ -10,9 +10,9 @@ class Shops(ClothShopFileEditor):
         ClothShopFileEditor.__init__(self, json_file, directory)
 
     @staticmethod
-    def convert_dict_to_dataclass(d, className):
+    def convert_dict_to_dataclass(d, class_name):
         """function to create tuple-like objects with named fields and fixed length"""
-        return className.from_dict(d)
+        return class_name.from_dict(d)
 
     def select_item_as_dataclass(self, input_name, input_color, input_size):
         """function selecting item from cloth_shop.json file by id"""
@@ -20,14 +20,19 @@ class Shops(ClothShopFileEditor):
             item = self.convert_dict_to_dataclass(d, ClothShop)
             if item.name == input_name:
                 for a in range(len(item.availability)):
-                    if item.availability[a].color == input_color and item.availability[a].size == input_size:
-                        if item.availability[a].quantity == 0:
-                            return (
-                                f"You selected {item.availability[a].color} {item.name} in size {item.availability[a].size} "
-                                f"that costs {item.price}$. Unfortunately, no such items are left in our shop.")
-                        else:
-                            return (
-                                f"You selected {item.availability[a].color} {item.name} in size {item.availability[a].size} "
-                                f"that costs {item.price}$. There are(is) {item.availability[a].quantity} item(s) in our shop.")
+                    if item.availability[a].color == input_color:
+                        if item.availability[a].size == input_size:
+                            if item.availability[a].quantity == 0:
+                                return (
+                                    f"You selected {item.availability[a].color} {item.name} in size {item.availability[a].size} "
+                                    f"that costs {item.price}$. Unfortunately, no such items are left in our shop.")
+                            else:
+                                return (
+                                    f"You selected {item.availability[a].color} {item.name} in size {item.availability[a].size} "
+                                    f"that costs {item.price}$. There are(is) {item.availability[a].quantity} item(s) in our shop.")
 
         return "Unfortunately, there is no such item in our shop."
+
+
+shop1 = Shops("cloth_shop.json", "assets")
+shop1.select_item_as_dataclass("Dress", "pink", "S")
--- tests/test_task_7_3.py
+++ tests/test_task_7_3.py
@@ -6,7 +6,9 @@ data = [
     ("Skirt", "red", "S", "You selected red Skirt in size S that costs 50$. There are(is) 3 item(s) in our shop."),
     ("Dress", "blue", "M",
      "You selected blue Dress in size M that costs 89$. Unfortunately, no such items are left in our shop."),
-    ("Coat", "blue", "M", "Unfortunately, there is no such item in our shop.")
+    ("Coat", "blue", "M", "Unfortunately, there is no such item in our shop."),
+    ("Dress", "red", "XS", "Unfortunately, there is no such item in our shop."),
+    ("Dress", "pink", "S", "Unfortunately, there is no such item in our shop.")
 ]
 
 
